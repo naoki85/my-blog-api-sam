49499fd92892cdfd6fea5fe75c8c5cd62dd0305b
naoki85 <naoki.yoneyama.85@gmail.com>
2019-10-02 21:55:14 +0900

    Use DynamoDb in repository

--- repository/recommended_book_repository.go
+++ repository/recommended_book_repository.go
@@ -3,6 +3,7 @@ package repository
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/dynamodb"
+	"github.com/aws/aws-sdk-go/service/dynamodb/dynamodbattribute"
 	"github.com/naoki85/my-blog-api-sam/model"
 	"log"
 	"os"
@@ -20,35 +21,35 @@ type RecommendedBookCreateParams struct {
 	ButtonUrl string
 }
 
-func (repo *RecommendedBookRepository) All(limit int) (recommendedBooks model.RecommendedBooks, err error) {
-	tableName := os.Getenv("RECOMMENDED_BOOKS_TABLE_NAME")
-	response, err2 := repo.DynamoDBHandler.Scan(&dynamodb.ScanInput{
-		TableName:            aws.String(tableName),
+func (repo *RecommendedBookRepository) tableName() (tableName string) {
+	if tableName = os.Getenv("RECOMMENDED_BOOKS_TABLE_NAME"); tableName != "" {
+		return tableName
+	} else {
+		return "RecommendedBooks"
+	}
+}
+
+func (repo *RecommendedBookRepository) All() (recommendedBooks model.RecommendedBooks, err error) {
+	result, err := repo.DynamoDBHandler.Scan(&dynamodb.ScanInput{
+		TableName:            aws.String(repo.tableName()),
 		ProjectionExpression: aws.String("Id, Link, ImageUrl, ButtonUrl"),
 	})
-	log.Printf("dynamodb: %v", response)
-	if err2 != nil {
-		log.Printf("dynamodbErr: %v", err2)
-	}
 
-	query := "SELECT id, link, image_url, button_url FROM recommended_books"
-	query = query + " ORDER BY id DESC LIMIT ?"
-	rows, err := repo.SqlHandler.Query(query, limit)
 	if err != nil {
-		log.Printf("%s", err.Error())
-		return recommendedBooks, err
+		log.Printf("dynamodbErr: %s", err.Error())
 	}
-	defer rows.Close()
 
-	for rows.Next() {
-		r := model.RecommendedBook{}
-		if err := rows.Scan(&r.Id, &r.Link, &r.ImageUrl, &r.ButtonUrl); err != nil {
-			log.Printf("%s", err.Error())
-			return recommendedBooks, err
+	for _, i := range result.Items {
+		book := model.RecommendedBook{}
+		err = dynamodbattribute.UnmarshalMap(i, &book)
+		if err != nil {
+			log.Println("Got error unmarshalling:")
+			log.Println(err.Error())
+			return
 		}
-
-		recommendedBooks = append(recommendedBooks, r)
+		recommendedBooks = append(recommendedBooks, book)
 	}
+
 	return
 }
 
--- repository/recommended_book_repository_test.go
+++ repository/recommended_book_repository_test.go
@@ -9,20 +9,16 @@ import (
 func TestShouldFindAllRecommendedBooks(t *testing.T) {
 	dynamoDbHandler, _ := testSupport.NewDynamoDbHandler()
 	mockSqlHandler, _ := NewMockSqlHandler()
-	mockSqlHandler.ResistMock("^SELECT (.+) FROM recommended_books .*", []string{"id", "link", "image_url", "button_url"})
 	repo := RecommendedBookRepository{
 		SqlHandler:      mockSqlHandler,
 		DynamoDBHandler: dynamoDbHandler,
 	}
-	recommendedBooks, err := repo.All(4)
+	recommendedBooks, err := repo.All()
 	if err != nil {
 		t.Fatalf("Cannot get recommended_books: %s", err)
 	}
-	if len(recommendedBooks) != 4 {
-		t.Fatalf("Fail expected: 4, got: %v", len(recommendedBooks))
-	}
-	if recommendedBooks[0].Link != "http://naoki85.test" {
-		t.Fatalf("Fail expected: http://naoki85.test, got: %v", recommendedBooks[0].Link)
+	if len(recommendedBooks) != 5 {
+		t.Fatalf("Fail expected: 5, got: %v", len(recommendedBooks))
 	}
 }
 
